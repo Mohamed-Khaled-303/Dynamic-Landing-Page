d05bb3e1091d60b6a77f3682b13d79dfa51bdf26
Mohamed-Khaled-303 <62609014+Mohamed-Khaled-303@users.noreply.github.com>
2022-04-22 15:15:45 +0200

    Adding an active state on the link when it is clicked.

--- js/app.js
+++ js/app.js
@@ -5,32 +5,40 @@ const sections = document.querySelectorAll('section'); //get a list of all secti
 
 // Building the navigation menu
 for (let section of sections) {
-    let listItemElement = document.createElement('li');//creating a new object <li></li>
-    listItemElement.innerHTML = `<a href="#${section.getAttribute('id')}" data-nav=${section.getAttribute('id')} class="menu__link">${section.getAttribute('data-nav')}</a>`;//Adding an ancor inside the <li></li>
+    let listItemElement = document.createElement('li'); //creating a new object <li></li>
+    listItemElement.innerHTML = `<a href="#${section.getAttribute('id')}" onclick="myFunction()" data-nav=${section.getAttribute('id')} class="menu__link">${section.getAttribute('data-nav')}</a>`; //Adding an ancor inside the <li></li>
     navBarList.appendChild(listItemElement) // appending the <li></li> to the navbar 
 }
+
+//Adding an active state on the link when it is clicked
+let links = document.querySelectorAll('nav a');
+links.forEach(link => {
+    link.addEventListener('click', function () {
+        links.forEach(lnk => lnk.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
 
 // Adding functionality to distinguish the section in view
 window.onscroll = () => {
     for (let section of sections) {
         let sectionPlace = section.getBoundingClientRect(); //store the section place in the veiw port in a variable 
-        if (sectionPlace.top >= -400 && sectionPlace.top <= 150) {//check weather the section is on the view port or not
-            section.classList.add('your-active-class');//making the section active
+        if (sectionPlace.top >= -400 && sectionPlace.top <= 150) { //check weather the section is on the view port or not
+            section.classList.add('your-active-class'); //making the section active
         } else {
-            section.classList.remove('your-active-class');//disactive the section 
+            section.classList.remove('your-active-class'); //disactive the section 
         }
     }
 }
 
 //Adding the functionality to scroll to sectionst
 navBarList.addEventListener("click", event => {
-    event.preventDefault();//prevent the default scrolling behavior
-    console.log(event.target.getAttribute('data-nav'));
+    event.preventDefault(); //prevent the default scrolling behavior
     if (event.target.getAttribute('data-nav')) {
         document
             .getElementById(`${event.target.getAttribute('data-nav')}`)
             .scrollIntoView({
-                behavior: "smooth"//smothing the scrolling
+                behavior: "smooth" //smothing the scrolling
             });
     }
 });
